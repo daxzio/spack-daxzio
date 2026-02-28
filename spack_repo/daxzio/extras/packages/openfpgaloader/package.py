c7753588b6353e283f30251f1f0b321d77b3ab55
Dave Keeshan <dave.keeshan@daxzio.com>
2026-02-28 11:36:01 +0000

    Add varient for openfpgaloader

---
+++ spack_repo/daxzio/extras/packages/openfpgaloader/package.py
@@ -0,0 +1,11 @@
+from spack.package import *
+from spack_repo.builtin.packages.openfpgaloader.package import (
+    Openfpgaloader as BuiltinOpenfpgaloader,
+)
+
+
+class Openfpgaloader(BuiltinOpenfpgaloader):
+    """openFPGALoader - extended with additional versions."""
+
+    # Custom version releases (add your own here)
+    version("2026.02.26", commit="3ae5e5e4a5f055d12f6079616cb1ec1d67bf30f1")
