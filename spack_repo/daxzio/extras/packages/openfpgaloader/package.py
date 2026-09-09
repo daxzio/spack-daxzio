838feaacea1f5fe7e8651e7588d33fcfcf2926e1
Dave Keeshan <dave.keeshan@daxzio.com>
2026-09-09 13:20:26 +0100

    Add a newer tag, 2025.09.09

--- spack_repo/daxzio/extras/packages/openfpgaloader/package.py
+++ spack_repo/daxzio/extras/packages/openfpgaloader/package.py
@@ -9,3 +9,4 @@ class Openfpgaloader(BuiltinOpenfpgaloader):
 
     # Custom version releases (add your own here)
     version("2026.02.26", commit="3ae5e5e4a5f055d12f6079616cb1ec1d67bf30f1")
+    version("2026.09.09", commit="f6a678b4bba17ffa93b67a4eb95bc9afa1ac1a36")
